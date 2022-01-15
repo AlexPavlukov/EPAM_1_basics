df204d3ec0966aef184079ef156f7548a67119c4
AlexPavlukov <alex@novoeradio.by>
2022-01-15 13:19:51 +0300

    changed names

--- linearprograms/Exercise01.java
+++ linearprograms/Exercise01.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class task1 {
+public class Exercise01 {
     private static final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
     public static void main(String[] args) throws IOException {
